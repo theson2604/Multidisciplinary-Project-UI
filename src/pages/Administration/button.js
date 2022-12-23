48c17843a3f4e189924aa16a15c988b2036d701e
WindyFeg <phongspeed7@yahoo.com.vn>
2022-12-23 20:35:48 +0700

    Admin can approve/decline

--- src/pages/Administration/button.js
+++ src/pages/Administration/button.js
@@ -1,27 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./index.css"
 
-function Button({ title }) {
+function Button({ title, postId, setHidenPost }) {
 
     const Approve = "Approve"
     const Decline = "Decline"
+
+    const ApproveHandle = () => {
+        // console.log("Approve call API for this" + postId)
+        const response = axios.get(`http://localhost:3000/admin/approve?postID=${postId}`)
+        setHidenPost(true)
+    }
+
+    const DeclineHandle = () => {
+        // console.log("Decline call API for this" + postId)
+        const response = axios.get(`http://localhost:3000/admin/:${postId}`)
+        setHidenPost(true)
+
+    }
+
     return (
-        title ?
-            <button className="btn2 btn-border-pop">
+        <button className={`${title ? "btnDecline" : "btnApprove"}`}
+            onClick={title ? DeclineHandle : ApproveHandle}
+        >
+            {
                 {
-                    {
-                        0: Approve,
-                        1: Decline,
-                    }[title]
-                }
-            </button> :
-            <button className="btn btn-border-pop">
-                {
-                    {
-                        0: Approve,
-                        1: Decline,
-                    }[title]
-                }
-            </button>
+                    0: Approve,
+                    1: Decline,
+                }[title]
+            }
+        </button>
     )
 }
 
--- src/pages/Administration/preview-post-admin.js
+++ src/pages/Administration/preview-post-admin.js
@@ -1,7 +1,22 @@
 import "./index.css"
 import Button from "./button"
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
 function PreviewPostAdmin({ previewInfo }) {
+
+    const [HidenPost, setHidenPost] = useState(false)
+
+    const navigate = useNavigate()
+
+    const showUser = () => {
+        console.log("Call user for " + previewInfo.authorDetail.lname)
+    }
+
+    const showPost = () => {
+        navigate("/post/" + previewInfo._id)
+        console.log("Call API for this post: " + previewInfo._id)
+    }
 
     const AuthorFullName =
         previewInfo.authorDetail.fname + " " + previewInfo.authorDetail.lname
@@ -10,13 +25,20 @@ function PreviewPostAdmin({ previewInfo }) {
         previewInfo.title : previewInfo.title.slice(0, 70) + "..."
 
     return (
-        <div className='prev-post-admin' >
-            <div className='prev-post-admin_container '>
+        <div className=
+            {`prev-post-admin 
+            ${HidenPost ? "prev-post-admin--hidden" : ""}`
+            }>
+            <div className='prev-post-admin_container'
+                onClick={showPost}
+            >
                 {PostTitle}
             </div>
             <div className="user-and-btn-box">
 
-                <div className='user-post'>
+                <div className='user-post'
+                    onClick={showUser}
+                >
                     <img
                         className='prev-post_uAvatar'
                         src={previewInfo.authorDetail.avatar}
@@ -24,8 +46,16 @@ function PreviewPostAdmin({ previewInfo }) {
                     {AuthorFullName}
                 </div>
                 <div className='administration-container'>
-                    <Button title={0} />
-                    <Button title={1} />
+                    <Button
+                        title={0}
+                        postId={previewInfo._id}
+                        setHidenPost={setHidenPost}
+                    />
+                    <Button
+                        title={1}
+                        postId={previewInfo._id}
+                        setHidenPost={setHidenPost}
+                    />
                 </div>
             </div>
         </div>
