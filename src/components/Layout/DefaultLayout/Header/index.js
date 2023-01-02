7928ca28324f57686f0d8e67744a97c08139ed7a
WindyFeg <phongspeed7@yahoo.com.vn>
2023-01-02 13:35:24 +0700

    Adding login/Signin btn on header

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -3,16 +3,58 @@ import clsx from 'clsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell, faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons';
 import useAuth from '../../../../hooks/useAuth';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+
 
 function Header() {
-    const {auth, setAuth} = useAuth()
+    const [isLogin, setIslogin] = useState(false)
+    const { auth, setAuth } = useAuth()
     const [name, setName] = useState('Username')
+    const navigate = useNavigate()
+
     //const currentUser = true
 
-    useEffect(() =>{
-        (name !== 'Username')? setName('Username') : setName(auth.fname)
+    useEffect(() => {
+        (name !== 'Username') ? setName('Username') : setName(auth.fname)
     }, [auth])
+
+    const LoginUI = () => {
+        navigate("/")
+        // setIslogin(true)
+    }
+
+    const SignInUI = () => {
+        navigate("/register")
+    }
+
+
+    const usernameHeader = <>
+        <button className={clsx(styles.actionButton)}>
+            <FontAwesomeIcon icon={faBell} />
+        </button>
+        <button className={clsx(styles.actionButton)}>
+            <FontAwesomeIcon icon={faUser} />
+        </button>
+        <div className={clsx(styles.username)}>{name}</div>
+    </>
+
+    const loginHeader = <>
+        <button className={clsx(styles.signInBtn)}
+            onClick={SignInUI}
+        >
+            <span className={clsx(styles.btnFont2)}>
+                Sign in
+            </span>
+        </button>
+        <button className={clsx(styles.loginBtn)}
+            onClick={LoginUI}
+        >
+            <span className={clsx(styles.btnFont)}>
+                Log in
+            </span>
+        </button> </>
+
 
     return (
         <header className={clsx(styles.wrapper)}>
@@ -21,19 +63,16 @@ function Header() {
                 <input placeholder='Search by ID or Name' spellCheck={false}
                 />
                 <button className={clsx(styles.searchButton)}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </button>
             </div>
             <div className={clsx(styles.actions)}>
-                <button className={clsx(styles.actionButton)}>
-                    <FontAwesomeIcon icon={faBell}/>
-                </button>   
-                <button className={clsx(styles.actionButton)}>
-                    <FontAwesomeIcon icon={faUser}/>
-                </button>
-                <div className={clsx(styles.username)}>{name}</div>
+                {isLogin === true ?
+                    usernameHeader
+                    : loginHeader
+                }
             </div>
-        </header>
+        </header >
     )
 }
 
